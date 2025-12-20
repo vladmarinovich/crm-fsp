da7ba93d3e63728df7346d036e9d6320254dd27a
Vladislav Marinovich <consultor@vladmarinovich.com>
2025-12-20 09:58:37 -0500

    fix: restore rel_donacion_filters variable definition to fix 500 error

--- backend/apps/core/views.py
+++ backend/apps/core/views.py
@@ -24,6 +24,7 @@ class DashboardView(APIView):
         
         # Filtros base
         donacion_filters = Q(estado__in=valid_statuses)
+        rel_donacion_filters = Q(donaciones__estado__in=valid_statuses)
         gasto_filters = Q() & ~Q(estado__in=['ANULADO', 'Anulado', 'anulado'])
         rel_gasto_filters = Q() & ~Q(gastos__estado__in=['ANULADO', 'Anulado', 'anulado'])
 
